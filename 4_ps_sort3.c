1d7d0955260054bce00b6817d5fd0c003c15de93
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-03-22 23:27:12 +0700

    <chore>: seperate files and clean push_swap.h main

--- 4_ps_sort3.c
+++ 4_ps_sort3.c
@@ -1,37 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ps_sort3.c                                         :+:      :+:    :+:   */
+/*   4_ps_sort3.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 19:31:35 by ktunchar          #+#    #+#             */
-/*   Updated: 2023/03/20 00:05:22 by ktunchar         ###   ########.fr       */
+/*   Updated: 2023/03/22 23:27:01 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_case1(t_stack **a)
+static int	ft_case1(t_stack **a)
 {
 	ft_sa(a);
 	return (1);
 }
 
-int	ft_case2(t_stack **a)
+static int	ft_case2(t_stack **a)
 {
 	ft_sa(a);
 	ft_rra(a);
 	return (2);
 }
 
-int	ft_case3(t_stack **a)
+static int	ft_case3(t_stack **a)
 {
 	ft_ra(a);
 	return (1);
 }
 
-int	ft_case5(t_stack **a)
+static int	ft_case5(t_stack **a)
 {
 	ft_rra(a);
 	return (1);
